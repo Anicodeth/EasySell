a9d7dc62e1d52019dd098bef22120505f7b16ff5
ffekirnew <ffekirnew0808@gmail.com>
2023-09-23 14:37:06 +0300

    Refactor main.py

--- src/main.py
+++ src/main.py
@@ -1,17 +1,8 @@
 from fastapi import FastAPI
 
-from WebApi.Controllers import product_controller, user_controller
+from src.webapi.controllers import product_controller, user_controller
 
 app = FastAPI()
 
-# Mount the user controller's router under the '/users' path
-app.include_router(user_controller.router, prefix="/users", tags=["Users"])
-
-# Mount the product controller's router under the '/products' path
-app.include_router(product_controller.router, prefix="/products", tags=["Products"])
-
-if __name__ == "__main__":
-    import uvicorn
-
-    # Run the FastAPI application using uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+app.include_router(user_controller.user_router)
+app.include_router(product_controller.product_router)
