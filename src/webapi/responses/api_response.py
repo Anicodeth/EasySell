13e281a434dc927319013bdb3a5c78ea2cb36b96
ffekirnew <ffekirnew0808@gmail.com>
2023-09-23 14:35:17 +0300

    Create a base api response class

--- src/webapi/responses/api_response.py
+++ src/webapi/responses/api_response.py
@@ -1,14 +1,15 @@
-from typing import TypeVar, Generic, Optional, Dict
-import json
+from typing import TypeVar, Optional, Generic
+
+from pydantic import BaseModel, Field
 
 T = TypeVar("T")
 
 
-class Response[Generic[T]]:
-    def __init__(self, status_code: str, message: str, data: Optional[T] = None):
-        self.status_code = status_code
-        self.message = message
-        self.data = data
+class ApiResponse(BaseModel):
+    success: bool = Field(...)
+    message: str = Field(...)
 
-    def to_json(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+class GenericResponse(ApiResponse, Generic[T]):
+    value: T = None
+    error: Optional[dict] = None
