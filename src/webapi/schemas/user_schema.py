b49e8d4488038ebbd597f70c2eba6d3bb6786b46
ffekirnew <ffekirnew0808@gmail.com>
2023-09-23 14:36:31 +0300

    Create schemas for webapi

--- src/webapi/schemas/user_schema.py
+++ src/webapi/schemas/user_schema.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 class UserBase(BaseModel):
     telegram_id: str
     telegram_username: str
-    phone_number: int
+    phone_number: str
 
 
 class UserCreate(UserBase):
