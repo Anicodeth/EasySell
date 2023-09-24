8c3348e29401b4670e83bbc221ac1b08aed31de5
ffekirnew <ffekirnew0808@gmail.com>
2023-09-24 10:10:38 +0300

    Move to another vercel configuration

---
+++ api/main.py
@@ -0,0 +1,2 @@
+# vercel will invoke this function for all routes as defined in vercel.json
+from src.main import app
--- src/main.py
+++ src/main.py
@@ -3,6 +3,5 @@ from fastapi import FastAPI
 from src.webapi.controllers import product_controller, user_controller
 
 app = FastAPI()
-
 app.include_router(user_controller.user_router)
 app.include_router(product_controller.product_router)
